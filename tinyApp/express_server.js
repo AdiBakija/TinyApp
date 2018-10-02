2decb1ef44c4f43788e38be7e0a26d472722f23b
Adi <adibakija35@hotmail.com>
2018-10-02 22:57:59 +0000

    completed setup of templates and pages

--- tinyApp/express_server.js
+++ tinyApp/express_server.js
@@ -19,6 +19,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  //console.log(req.params)
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
