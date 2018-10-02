f9d0b3ea6762f16f274bdcc27f1066fbf50bc230
Adi <adibakija35@hotmail.com>
2018-10-02 23:26:19 +0000

    Added random string generator function and get/post for newurl

--- tinyApp/express_server.js
+++ tinyApp/express_server.js
@@ -1,11 +1,19 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 //This ist he URL data that needs to get passes to urls_index.ejs
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  let r = Math.random().toString(36).substring(7);
+  console.log("random", r);
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -17,6 +25,15 @@ app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase};
   //"urls_index is actually a template of ejs"
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
